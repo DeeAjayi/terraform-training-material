cfc8cdf5f246b23b475c7b8b5cf59c320938bbea
Oladipupo Ajayi <ajayidipo@ymail.com>
2020-11-19 08:25:13 +0100

    first full commit

--- files/custom-data.sh
+++ files/custom-data.sh
@@ -5,5 +5,5 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
 sudo apt-get install -y python3-pip
 pip install boto boto3
-git clone https://DeeAjayi:f46a2af83aef3632d21f8cebd1e78f51f598ac22@github.com/DeeAjayi/telios-aws-test /home/devops/demo
+git clone https://DeeAjayi:718ecf8b63b3fde1205da9a62bf7adf0552fb6c2@github.com/DeeAjayi/telios-aws-test /home/devops/demo
 ansible-playbook -c local /home/devops/demo/install-nginx.yaml
--- vm.tf
+++ vm.tf
@@ -24,6 +24,42 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     disable_password_authentication = false
     custom_data = base64encode(file("files/custom-data.sh"))
 
+    # provisioner "file" {
+    # source      = "./index.html"
+    # destination = "./index.html"
+
+    #     connection {
+    #         type     = "ssh"
+    #         user     = "azureuser"
+    #         password = "azureuser123$"
+    #         host     = azurerm_public_ip.myterraformpublicip.ip_address
+    #     }
+    # }
+
+    # provisioner "file" {
+    # source      = "./nginx.yaml"
+    # destination = "./nginx.yaml"
+
+    #     connection {
+    #         type     = "ssh"
+    #         user     = "azureuser"
+    #         password = "azureuser123$"
+    #         host     = azurerm_public_ip.myterraformpublicip.ip_address
+    #     }
+    # }
+
+    # provisioner "remote-exec" {
+    #     inline = [
+    #       "sudo apt-get update",
+    #       "sudo apt-get install docker.io -y",
+    #       "git clone https://github.com/somepublicrepo.git",
+    #       "cd Docker-sample",
+    #       "sudo docker build -t mywebapp .",
+    #       "sudo docker run -d -p 5000:5000 mywebapp"
+    #     ]
+    # }
+
+
     boot_diagnostics {
         storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
